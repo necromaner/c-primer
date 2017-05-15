0a40952ce5c0da2e53290308a275c4f1d60ec9ac
necromaner <necromaner@foxmail.com>
2017-05-15 22:56:34 +0800

    17.5.15

--- Chapter Five/5.3.1/5.6/main.cpp
+++ Chapter Five/5.3.1/5.6/main.cpp
@@ -1,3 +1,4 @@
+//5.6：改写上一题的程序，使用条件运算符（参见4.7节，第134页）代替ifelse语句。
 //5.5：写一段自己的程序，使用ifelse语句实现把数字转换成字母的要求。
 
 #include <iostream>
@@ -15,14 +16,15 @@ int main() {
     int i;
     cout<<"enter number(1 to 100):";
     cin>>i;
+    b=(i<60)?scores[0]:(i>100)?"error":scores[(i-50)/10];
+    /*
     if(i<60)
         b=scores[0];
     else if(i>100)
         b={"error"};
     else
         b=scores[(i-50)/10];
-
+*/
     cout<<"level is "<<b<<endl;
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
