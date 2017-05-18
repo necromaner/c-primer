811ad34718dc5ec1839aef74f86e733c320d21a5
necromaner <necromaner@foxmail.com>
2017-05-18 17:47:39 +0800

    5.18

--- Chapter Five/5.4.4/5.18/main.cpp
+++ Chapter Five/5.4.4/5.18/main.cpp
@@ -1,6 +1,45 @@
+/*5.18：说明下列循环的含义并改正其中的错误。
+
+（a）
+ do
+    int v1, v2;
+    cout <<"please enter two numbers to sum:";
+    if (cin >> v1 >> v2)
+        cout <<"sum is: " << v1 + v2 << endl;
+    while (cin);
+(b)
+do {
+//...
+}while (int ival = get_response());
+
+(c)
+do {
+int ival = get_response();
+} while (ival);
+*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    do {                //缺少花括号
+        int v1, v2;
+        cout << "please enter two numbers to sum:";
+        if (cin >> v1 >> v2)
+            cout << "sum is: " << v1 + v2 << endl;
+    }
+    while (cin);
+
+    int ival;
+    do {
+//...
+
+    }while(ival = get_response());//变量要声明在do的条件部分
+
+    int ival2;
+    do {
+        ival2 = get_response();
+    } while (ival2);//ival是局部变量，出了括号，while不认识了
     return 0;
 }
