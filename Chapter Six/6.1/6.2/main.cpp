ffc26938dd90dedaa19cce936736f5226127e3b7
necromaner <necromaner@foxmail.com>
2017-05-22 22:38:38 +0800

    5.22

---
+++ Chapter Six/6.1/6.2/main.cpp
@@ -0,0 +1,30 @@
+/*6.2：请指出下列函数哪个有错误，为什么？应该如何修改这些错误呢？
+
+(a). int f()
+{
+    string s;
+    //…
+    return s;
+}
+
+(b) f2(int i){/ *…* /}
+(c) int calc(int v1, int v1) / *…* / }
+(d) double square(double x) return x*x;
+*/
+
+#include <iostream>
+
+string f()//a.
+{
+    string s;
+    //…
+    return s;
+}
+int f2(int i){/*…*/}//c.缺int
+
+int calc(int v1, int v2) {/*…*/}//b.不能有两个相同的行参名
+
+double square(double x) {return x*x;}//c.无大括号
+int main() {
+    return 0;
+}
