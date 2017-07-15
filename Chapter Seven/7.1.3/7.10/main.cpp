1569575879b16bf49156e8f99288c29d63fae2d5
necromaner <necromaner@foxmail.com>
2017-07-15 22:19:46 +0800

    7.15

--- Chapter Seven/7.1.3/7.10/main.cpp
+++ Chapter Seven/7.1.3/7.10/main.cpp
@@ -1,6 +1,7 @@
 //7.10：在下面if语句中，条件部分的作用是什么？
-//if(read(read(cin,data1),data2)
+//if(read(read(cin,data1),data2))
 
+//答:read(cin,data1)向data1输入内容，read(read(cin,data1),data2)把data1内容输入到data2里，if(read(read(cin,data1),data2))如果值为0为假
 #include <iostream>
 #include <string>
 
@@ -25,7 +26,6 @@ int main() {
     Person data1;
     Person data2;
     if(read(read(cin,data1),data2)){
-        cout<<"yes";
     }
         return 0;
 }
